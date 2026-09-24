26542cd877677e23474c697493293709fec287c9
pnagaev <pnagaev@yandex.ru>
2026-09-24 08:52:07 +0300

    Update Microsoft.PowerShell_profile.ps1

--- console/Microsoft.PowerShell_profile.ps1
+++ console/Microsoft.PowerShell_profile.ps1
@@ -1,10 +1,14 @@
+Write-Host "Custom profile loaded" -ForegroundColor green
+cd c:\scripts
+
+# Поиск по истории(как в Linux): введите часть команды и выбирайте стрелками только по введённым символам
 Set-PSReadLineKeyHandler -Key UpArrow   -Function HistorySearchBackward
 Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
 
-Write-Host "Custom profile loaded" -ForegroundColor green
+# Обновляем вывод get-mailbox на более информативный
 Update-FormatData -PrependPath C:\scripts\myexchange.ps1xml
-cd c:\scripts
 
+# Алиас ll(как в Linux), часто набираю вместо ls
 function ll {
     Get-ChildItem -Force @args | Select-Object Mode, LastWriteTime, @{
         Name = 'Size'
